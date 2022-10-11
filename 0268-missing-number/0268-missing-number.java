6f5d3bd3cb64d4ad889579d130aa688cc995ab79
SHIVAM VISHWAKARMA <vishwakshivam@gmail.com>
2022-10-12 01:30:46 +0530

    Time: 3 ms (31.98%), Space: 53.3 MB (8.94%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,19 +2,30 @@ class Solution {
     
     public int missingNumber(int[] nums) {
         
-        int length = nums.length;
-        int Totalsum =0;
-        for(int i=0;i<=length;i++)
-        {
-            Totalsum = Totalsum+ i;
-        }
-        int lessSum =0;
+//         int length = nums.length;
+//         int Totalsum =0;
+//         for(int i=0;i<=length;i++)
+//         {
+//             Totalsum = Totalsum+ i;
+//         }
+//         int lessSum =0;
         
-        for(Integer s : nums)
-        {
-            Totalsum = Totalsum - s;
+//         for(Integer s : nums)
+//         {
+//             Totalsum = Totalsum - s;
+//         }
+//         return  Totalsum;
+        // XOR Operation
+          int length = nums.length;
+        int xorValue = 0;
+        for (int i = 0; i <= length; i++) {
+            xorValue = xorValue ^ i;
         }
-        return  Totalsum;
+
+        for (Integer a : nums) {
+            xorValue = xorValue ^ a;
+        }
+return xorValue;
         
     }
 }
