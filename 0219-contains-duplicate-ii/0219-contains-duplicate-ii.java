adfb52e1e760a77f1bb7617bddea826f2b350203
SHIVAM VISHWAKARMA <vishwakshivam@gmail.com>
2022-10-12 00:06:58 +0530

    Time: 60 ms (26.70%), Space: 97 MB (6.83%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -8,8 +8,10 @@ class Solution {
         }
 
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i]) && (i - map.get(nums[i])) <= k) {
-                return true;
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                {
+                    return true;
+                }
             } else {
                 map.put(nums[i], i);
             }
