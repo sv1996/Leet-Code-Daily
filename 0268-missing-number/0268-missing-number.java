33de15f23745ebb3413e4dc59c0e0d6586dfe974
SHIVAM VISHWAKARMA <vishwakshivam@gmail.com>
2022-10-12 02:55:27 +0530

    Time: 24 ms (5.30%), Space: 54.6 MB (5.68%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,31 +1,52 @@
 class Solution {
-    
+
     public int missingNumber(int[] nums) {
-        
-//         int length = nums.length;
-//         int Totalsum =0;
-//         for(int i=0;i<=length;i++)
-//         {
-//             Totalsum = Totalsum+ i;
-//         }
-//         int lessSum =0;
-        
-//         for(Integer s : nums)
-//         {
-//             Totalsum = Totalsum - s;
-//         }
-//         return  Totalsum;
+        //         int length = nums.length;
+        //         int Totalsum =0;
+        //         for(int i=0;i<=length;i++)
+        //         {
+        //             Totalsum = Totalsum+ i;
+        //         }
+        //         int lessSum =0;
+
+        //         for(Integer s : nums)
+        //         {
+        //             Totalsum = Totalsum - s;
+        //         }
+        //         return  Totalsum;
         // XOR Operation
-          int length = nums.length;
-        int xorValue = 0;
+        //           int length = nums.length;
+        //         int xorValue = 0;
+        //         for (int i = 0; i <= length; i++) {
+        //             xorValue = xorValue ^ i;
+        //         }
+
+        //         for (Integer a : nums) {
+        //             xorValue = xorValue ^ a;
+        //         }
+        // return xorValue;
+
+        // using map
+        int length = nums.length;
+        HashMap<Integer, Boolean> map = new HashMap<>();
         for (int i = 0; i <= length; i++) {
-            xorValue = xorValue ^ i;
+            map.put(i, false);
         }
 
-        for (Integer a : nums) {
-            xorValue = xorValue ^ a;
+        for (int a : nums) {
+            if (map.containsKey(a)) {
+                map.put(a, true);
+            }
         }
-return xorValue;
+int ans = Integer.MIN_VALUE;
+        for (Map.Entry a : map.entrySet())
+        {
+            if ((Boolean) a.getValue() == false)
+            {
+                ans= (Integer)a.getKey();
+            }
+        }
         
+ return ans;
     }
 }
